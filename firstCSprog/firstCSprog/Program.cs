198d88c5e72aecfddab8a4b1ee4dd2e58c4e1a5d
September <vitaliiseptember@gmail.com>
2019-04-22 14:12:45 +0300

    first c# prog

---
+++ firstCSprog/firstCSprog/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace firstCSprog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Привет GitHub!");//Проверка кодировки
+            Console.Read();
+        }
+    }
+}
