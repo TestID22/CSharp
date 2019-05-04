331509bfbc245213169b164ec36eae69ce0d55a3
September <vitaliiseptember@gmail.com>
2019-05-04 17:40:25 +0300

    leran C#

---
+++ Shildt_simple_code/Shildt_simple_code/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shildt_simple_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hi_to_shildt;
+            Console.WriteLine("Привет ШИЛДТ, я тут твой С# учу");
+            Console.WriteLine("Введите сообщения для Шилдта");
+            Console.Read();
+        }
+    }
+}
---
+++ compiler/compiler/compiler/CodeFile1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programm {
+
+    static void Main()
+    {
+        int x = 10;
+        int y;
+        double dvar = 100;// тип данных с плавающей точкой
+        
+            Console.WriteLine("X =  " + x);
+            y = x / 2;
+            Console.WriteLine("Y равен x / 2 = " + y);
+            Console.WriteLine(y);
+        Console.WriteLine();
+            dvar = dvar / 3;
+        Console.WriteLine(dvar);
+    }
+
+}
