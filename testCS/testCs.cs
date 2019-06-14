7d00a5e84f3572fac3ff2d94b67c164bd95a235b
septemberEatMe <vitaliiseptember@gmail.com>
2019-06-14 09:50:07 +0300

    learn c#

---
+++ testCS/testCs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Test
+{
+    public static void Main(string[] argv)
+    {
+        
+        string name = Console.ReadLine();
+        
+        for (int i = name.Length -1; i < name.Length; i--)
+            Console.Write(name[i]);
+
+        
+    }
+}
