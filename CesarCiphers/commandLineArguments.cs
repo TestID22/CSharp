53a9c9a07e11af978773ba3a7a7d9c9630fae7f8
septemberEatMe <vitaliiseptember@gmail.com>
2019-08-18 16:07:52 +0300

    Cesar Algo

---
+++ CesarCiphers/commandLineArguments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+/**
+ * Шифо Цезаря ...
+ */
+
+class SystemArguments
+{
+    public static void Main(String[] args)
+    {
+        if (args.Length < 2)
+            Console.WriteLine("Введите з для шифровки, вторым аргументом нужно ввести слово");
+
+        else if (args[0] != "з" && args[0] != "д")
+            Console.WriteLine("Неверно введён флаг");
+
+        else
+            for (int n = 1; n < args.Length; n++)
+                for (int i = 0; i < args[n].Length; i++)
+                    if (args[0] == "з")
+                        Console.WriteLine((char)(args[n][i] + 1));
+                    else if (args[0] == "д")
+                        Console.WriteLine((char)(args[n][i] - 1));
+                    else
+                        foreach (String s in args)
+                            Console.WriteLine(args[i]);
+
+        Console.ReadLine(); 
+    }
+    
+}
