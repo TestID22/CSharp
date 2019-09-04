133ea54e40361608bfb58b2228570e1ae3983951
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-04 23:31:04 +0300

    CodeBlogTask 1

---
+++ codeBlogNeo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace codeBlog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine("Wake up Neo...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Follow the white Rabbit");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Knock Knock NEO...");
+            Console.ReadLine();
+
+        }
+    }
+}
