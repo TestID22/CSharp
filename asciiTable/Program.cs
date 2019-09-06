75b0b25971519c444694b094f69c91f781b62c36
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-06 13:23:52 +0300

    create ASCII TableCoding type char

---
+++ asciiTable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asciiTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ASCII TABLE");
+            for(int i = 0; i < 255; i++)
+            {
+                Console.WriteLine("Номер кода: " + i + "\t СИМВОЛ: " + (char)i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
