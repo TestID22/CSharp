111461ff8dbb92b3fb7ec92cb79906ffa588eb26
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-08 12:44:32 +0300

    GET OS INFO

---
+++ showOSparameters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showOSparameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 60;
+            ShowInfo();
+
+            Console.ReadLine();
+        }
+
+        static void ShowInfo()
+        {
+            foreach(string drive in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine("Drive {0}", drive);
+            }
+
+            Console.WriteLine("OS: " +  Environment.OSVersion);
+            Console.WriteLine("Колво процессоров " +  Environment.ProcessorCount);
+            Console.WriteLine("Нет платформа " +  Environment.Version);
+            
+        }
+    }
+}
