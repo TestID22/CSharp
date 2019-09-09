46fdceffb457036871404494163c91ac3e66a71a
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-09 13:37:34 +0300

    Data Structure

---
+++ arratListEnum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arratListEnum
+{
+    class Program
+    {
+            //Перечисления ENUM
+            enum Romanic
+            {
+                I,
+                II,
+                III,
+                IV,
+                V,
+                VI,
+                VII,
+                VIII,
+                IX
+            }
+
+        static void Main(string[] args)
+        {
+            //двумерный массив
+            int[,] ages = new int[10,10];
+            ages[0,0] = 10;
+            ages[9,9] = 1;
+            Console.WriteLine(ages[0, 1]);
+
+
+            //Список 
+            List<string> hackers = new List<string>(10);
+
+            hackers.Add("Elliot");
+            hackers.Add("Darling");
+            Console.WriteLine(hackers.ElementAt(1));
+
+            Console.WriteLine(Romanic.IX);
+            Console.ReadLine();
+        }
+    }
+}
