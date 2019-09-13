4f4abc03fa4b10156dc6301664895080f6c0e18c
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-13 13:37:32 +0300

    easy way for Getter Setter

---
+++ Properties/Hacker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Hacker
+    {
+        private string hackersName;
+        private int hackersSkill;
+        private string age;
+
+        //Properties
+        public int healthPoint { get; set; } //Auto Properties
+        public string power { get; set; }
+        public string Name
+        {
+            get { return hackersName; }
+            set
+            {
+                hackersName = value;
+            }
+
+
+        }
+        public int Skill
+        {
+            get { return hackersSkill; }
+            set { hackersSkill = value; }
+
+        }
+        public string Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        
+        public Hacker() {} //создадим стандартный "пустой" экземпляр
+
+        public Hacker(string hackerName, string power) //объект с двумя параметрами
+            :this(hackerName, 0, 0, "0", power) { }
+
+        public Hacker(string hackersName, int hackersSkill, int healthpoint)
+            :this(hackersName, hackersSkill, 100, "", ""){ }
+
+
+        public Hacker(string hackerName, int hackersSkill, int healthPoint, string age, string power)
+        {
+            this.hackersName = hackerName;
+            this.hackersSkill = hackersSkill;
+            this.healthPoint = healthPoint;
+            this.age = age;
+            this.power = power;
+        }
+    }
+}
---
+++ Properties/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cоздадим 3 объекта класса Хакер, искользуя 3 перегруженных конструктора
+            Hacker elliot = new Hacker();
+            Hacker darling = new Hacker("Darling", "Girl Empaty");
+            Hacker tyrell = new Hacker("Tyrell", 100, 100, "34", "Hide");
+
+            elliot.Name = "Elliot";
+            elliot.Skill = 99;
+            elliot.healthPoint = 50;
+
+            List<Hacker> fsociety = new List<Hacker>(3);
+            fsociety.Add(elliot);
+            fsociety.Add(darling);
+            fsociety.Add(tyrell);
+
+            foreach (Hacker h in fsociety)
+            {
+                Console.WriteLine("Name: {0} \nSkill: {1}\nHP:{2}",
+                    h.Name, h.Skill, h.healthPoint, h.power);
+                Console.WriteLine();
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
