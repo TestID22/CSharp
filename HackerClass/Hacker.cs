22c3b9ba62bdeb64936e33ed09ff85be994bf38d
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-13 13:36:41 +0300

    rename only

---
+++ HackerClass/Hacker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace createClass
+{
+    class Hacker
+    {
+        private string name;
+
+        public Hacker(string name)
+        {
+            this.name = name;
+        }
+        public void hackCoorpEvil()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            string logInSystem = "login:Eliot";
+            char[] charLog = logInSystem.ToCharArray();
+            Thread.Sleep(1000);
+
+            for(int i = 0; i < charLog.Length; i++)
+            {
+                
+                 Console.Write(charLog.ElementAt(i));
+                 Thread.Sleep(100);
+            }
+
+            Console.Write("\n");
+            Console.WriteLine("password:*******");
+        }
+        public void GoToTyrell()
+        {
+            Console.WriteLine("Иду к Тайлеру");
+        }
+    }
+}
---
+++ HackerClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace createClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hacker Elliot = new Hacker("Elliot");
+            
+            Console.WriteLine("1:Взломать Корпроацию зла");
+            int handle = Int32.Parse(Console.ReadLine());
+            switch(handle)
+            {
+                case (1):
+                    Elliot.hackCoorpEvil();
+                    break;
+                case(2):
+                    Elliot.GoToTyrell();
+                    break;
+                default:
+                    Console.WriteLine("Введите команду");
+                    
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
