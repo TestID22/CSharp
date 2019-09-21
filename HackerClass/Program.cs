f33de6ca9b93bf5bf0cc32db04162da40a7aaec2
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-21 14:26:23 +0300

    update ico

--- HackerClass/Program.cs
+++ HackerClass/Program.cs
@@ -10,26 +10,33 @@ namespace createClass
     {
         static void Main(string[] args)
         {
-            Hacker Elliot = new Hacker("Elliot");
-            
-            Console.WriteLine("1:Взломать Корпроацию зла");
-            int handle = Int32.Parse(Console.ReadLine());
-            switch(handle)
+            unsafe
             {
-                case (1):
-                    Elliot.hackCoorpEvil();
-                    break;
-                case(2):
-                    Elliot.GoToTyrell();
-                    break;
-                default:
-                    Console.WriteLine("Введите команду");
-                    
-                    break;
-            }
+                int a = 10;
+                int* p = &a;
+                Console.WriteLine("{0}", *p);
 
-            Console.WriteLine(Elliot.ToString());
-            Console.ReadLine();
+                Hacker Elliot = new Hacker("Elliot");
+
+                Console.WriteLine("1:Взломать Корпроацию зла");
+                int handle = Int32.Parse(Console.ReadLine());
+                switch (handle)
+                {
+                    case (1):
+                        Elliot.hackCoorpEvil();
+                        break;
+                    case (2):
+                        Elliot.GoToTyrell();
+                        break;
+                    default:
+                        Console.WriteLine("Введите команду");
+
+                        break;
+                }
+
+                Console.WriteLine(Elliot.ToString());
+                Console.ReadLine();
+            }
         }
     }
 }
