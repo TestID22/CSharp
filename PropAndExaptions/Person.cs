0eabd8fb28652fad29efb5252734ca8d5b16ed07
septemberEatMe <vitaliiseptember@gmail.com>
2019-09-22 17:13:39 +0300

    Properties

---
+++ PropAndExaptions/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropAndExaptions
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public void SetName(string Name)
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                throw new ArgumentNullException("Введите имя");
+            }
+
+            this.Name = Name;
+        }
+    }
+}
---
+++ PropAndExaptions/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropAndExaptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.SetName("Вася");
+            Console.WriteLine(p.Name);
+            Console.ReadLine();
+        }
+        
+    }
+}
