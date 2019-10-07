1f8576eff4e0933e772705e270d1437875ad4f08
septemberEatMe <vitaliiseptember@gmail.com>
2019-10-07 17:43:12 +0300

    Kill Start Process open URL

---
+++ PtocessKill/KillerP.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PtocessKill
+{
+    static class KillerP
+    {
+
+        public static void LoadAllProcess()
+        {
+            var allProcess = from proc in Process.GetProcesses(".") 
+                             orderby proc.Id
+                             select new { proc.ProcessName, proc.Id };
+
+
+            int numbers = 0;
+            foreach(var p in allProcess)
+            {
+                Console.WriteLine("[{0}]  {1} {2}", numbers, p.Id, p.ProcessName);
+                numbers++;
+                
+            }
+        }
+        
+        public static void GetProcess(int id)
+        {
+            Process process = null;
+            try
+            {
+                process = Process.GetProcessById(id);
+                
+                process.Kill();
+
+                Console.WriteLine("Процесс {0} убит", process.ProcessName);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+        }
+
+        public static void Start(string startProcessName, string url)
+        {
+            //TODO: проверка.
+            if (string.IsNullOrWhiteSpace(startProcessName))
+            {
+                throw new ArgumentNullException("имя процесса не может быть Null");
+            }
+
+            Process startProcess = null;
+            try
+            {
+                startProcess = Process.Start(startProcessName, url);
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ PtocessKill/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PtocessKill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            KillerP.LoadAllProcess();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            Console.WriteLine("Введите процесс который мы жестоко убъём");
+            int processIdNumber = int.Parse(Console.ReadLine());
+            KillerP.GetProcess(processIdNumber);
+
+            OpenUrlInBrows();
+            Console.ReadLine();
+        }
+
+        private static void OpenUrlInBrows()
+        {
+            Console.WriteLine("Введите ваш браузер и сайт");
+            string startProcessName = Console.ReadLine();
+            string url = Console.ReadLine();
+            KillerP.Start(startProcessName, url);
+        }
+    }
+
+}
