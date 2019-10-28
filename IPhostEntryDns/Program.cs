79b99268200833c728ab408063c3c914bb9ace35
septemberEatMe <vitaliiseptember@gmail.com>
2019-10-28 13:02:16 +0200

    serverNameDetecter/html parser

--- IPhostEntryDns/Program.cs
+++ IPhostEntryDns/Program.cs
@@ -10,34 +10,61 @@ namespace IPhostEntryDns
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             //получить айпиАдресса всех серверов сайта
             IPHostEntry host = Dns.GetHostEntry("www.google.com");
-            foreach(var h in host.AddressList)
+            foreach (var h in host.AddressList)
             {
                 Console.WriteLine(h);
             }
+            //TODO: научится вставлять свои заголовки
+            //GetHtml("http://www.google.com");
 
+
+            await GetServerName("http://www.girnyk.dn.ua");
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+        //определить сервер
+        private static async Task GetServerName(string url)
+        {
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            WebHeaderCollection headers = response.Headers;
+            for (int i = 0; i < headers.Count; i++)
+            {
+                if (headers.GetKey(i).ToLower().Contains("server"))
+                    Console.WriteLine("{0}: {1}", headers.GetKey(i), headers[i]);
+            }
+            response.Close();
+        }
+
+        //Получим html страницы
+        private static void GetHtml(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                throw new Exception("URL не может быть пуст или null");
+            }
             //request к сайту и симпл парсер
-            WebRequest req = WebRequest.Create("http://www.google.com");
+            WebRequest req = WebRequest.Create(url);
             WebResponse res = req.GetResponse();
-            using(Stream stream  = res.GetResponseStream())
+            using (Stream stream = res.GetResponseStream())
             {
                 using (StreamReader reader = new StreamReader(stream))
                 {
                     string line = "";
-                    while ((line = reader.ReadLine())!=null)
+                    while ((line = reader.ReadLine()) != null)
                     {
                         Console.WriteLine(line);
-                       
+
                     }
                 }
                 res.Close();
             }
-
-            Console.WriteLine();
-            Console.ReadLine();
         }
     }
 }
