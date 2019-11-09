08ba022af7f7b54a7592ff08d2d9c3891cb020d3
septemberEatMe <vitaliiseptember@gmail.com>
2019-11-09 11:53:43 +0200

    workWithDelegateAttentionPLS

---
+++ DelegateMetanitFuncPointersLambda/FuncParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DelegateMetanitFuncPointersLambda
+{
+    public class FuncPointerParameters
+    {
+        public delegate bool IsTrue(int i);
+
+
+        public static int Add(int[] integers, IsTrue del)
+        {
+            int result = 0;
+            foreach(int i in integers)
+            {
+                if (del(i))
+                {
+                    result += i;
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ DelegateMetanitFuncPointersLambda/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DelegateMetanitFuncPointersLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Lambda Inside
+            int[] intArray = { 1, 6, 12, 9, 0 };
+            //на место парметра делегата, можно пердать лямбду, или анонимный метод, что равносильно указателю на функцию в C.
+            int resultFromLambda = FuncPointerParameters.Add(intArray, x => x < 6);
+            Console.WriteLine(resultFromLambda);
+            #endregion
+
+            #region MethodToFunc Inside
+            int resultFromMethod = FuncPointerParameters.Add(intArray, GetNum);
+            Console.WriteLine(resultFromMethod);
+            #endregion
+
+            Console.ReadLine();
+        }
+
+        public static bool GetNum(int x)
+        {
+            if (x > 10)
+            {
+                return true;
+            }
+            else return false;
+        }
+
+
+
+    }
+}
