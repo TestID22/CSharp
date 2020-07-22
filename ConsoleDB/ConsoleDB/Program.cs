38601db7055f559778253865ca181504739bda70
Vitaliy Sabadash <vitaliiseptember@gmail.com>
2020-07-22 19:07:08 +0300

    try DB in console

---
+++ ConsoleDB/ConsoleDB/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Virus");
+            Console.ReadLine();
+        }
+    }
+}
